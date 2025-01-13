bd3ee38bfff5b2046ed13bc27cbe9186ebb09d27
Kevin Arthur <kevin.arthur.roy@gmail.com>
2025-01-13 15:20:28 -0800

    Implement ability to rotate player left and right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
@@ -12,12 +13,20 @@ from constants import (
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
